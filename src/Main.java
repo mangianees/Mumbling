d66b13981cbb39e00a9246227d1ff11349c93846
mangianees@gmail.com <mangianees@gmail.com>
2023-12-26 10:29:39 +0000

    Completed

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "abcd";
+        for(int i=0; i<str.length();i++)
+            for ( int j=0;j<=i;j++)
+                System.out.print(str.charAt(i));
+
+
+    }
+}
